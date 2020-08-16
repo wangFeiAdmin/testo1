4451f8c3ca9be18017a155496b7ac9e0d3d5a96f
wf <wf@hp.com>
2020-08-16 12:14:42 +0800

    第三个测试

--- src/test/java/com/wf/test/testDemo.java
+++ src/test/java/com/wf/test/testDemo.java
@@ -4,6 +4,10 @@ public class testDemo {
     public static void main(String [] args){
         System.out.println("hello,word");
         System.out.println("hellowrd ,two");
+
+        System.out.println("此处先不进行更新的操作试试看看会不会出现版本冲突的问题");
+
+
     }
 
 }
