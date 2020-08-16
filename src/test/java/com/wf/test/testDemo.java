42a053a01827adb40cbe0aaf4b7a533ca19898ce
wf <wf@hp.com>
2020-08-16 12:00:05 +0800

    第二个版本

--- src/test/java/com/wf/test/testDemo.java
+++ src/test/java/com/wf/test/testDemo.java
@@ -3,6 +3,7 @@ package com.wf.test;
 public class testDemo {
     public static void main(String [] args){
         System.out.println("hello,word");
+        System.out.println("hellowrd ,two");
     }
 
 }
